5b8e7edd27ba3343c20e78fafc07d031b565041b
Ulises Moreno <Ulises Moreno@DESKTOP-L6DR3GS>
2019-07-12 11:17:35 -0500

    Subir Solucion

---
+++ DemoApp/Models/Videojuego.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoApp.Models
+{
+    public class Videojuego
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string ImgUrl { get; set; }
+
+    }
+}
